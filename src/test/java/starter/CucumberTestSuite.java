6097108335f805614513226a62474e1b3541130a
unknown <evan.a.west@gmail.com>
2023-06-11 16:27:24 -0400

    Updated version of serenity to 3.8 and serenity gradle plugin to 3.7

--- src/test/java/starter/CucumberTestSuite.java
+++ src/test/java/starter/CucumberTestSuite.java
@@ -5,8 +5,6 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(
-        plugin = {"pretty"},
-        features = "src/test/resources/features"
-)
-public class CucumberTestSuite {}
+@CucumberOptions(plugin = { "pretty" }, features = "src/test/resources/features")
+public class CucumberTestSuite {
+}
