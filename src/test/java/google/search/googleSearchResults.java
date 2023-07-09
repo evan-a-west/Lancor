f87466f4c6c71ef74900aa50183887678a55e4cf
unknown <evan.a.west@gmail.com>
2023-07-08 20:19:01 -0400

    Save sample CSS for site name on Google search

--- src/test/java/google/search/googleSearchResults.java
+++ src/test/java/google/search/googleSearchResults.java
@@ -4,7 +4,7 @@ import net.serenitybdd.screenplay.targets.Target;
 
 public class googleSearchResults {
     private static final String usefulSearchResults = "div.v7W49e>div>div.g %s, div.v7W49e>div>div>div.g %s, div.v7W49e>div>div>div>div.g %s";
-    
+    //Example CSS for Search Result -> Site Name: "div.v7W49e>div>div.g div.TbwUpd.NJjxre.iUh30.apx8Vc.ojE3Fb span.VuuXrf, div.v7W49e>div>div>div.g div.TbwUpd.NJjxre.iUh30.apx8Vc.ojE3Fb span.VuuXrf, div.v7W49e>div>div>div>div.g div.TbwUpd.NJjxre.iUh30.apx8Vc.ojE3Fb span.VuuXrf"
     
     //General Google Search Result fields
     public static final Target GENERAL_RESULT__HEADER =  Target.the("Search Result Header").locatedBy(".LC20lb.MBeuO.DKV0Md");
