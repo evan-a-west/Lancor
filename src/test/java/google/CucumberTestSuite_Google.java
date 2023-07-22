b0ac86270013f59b9e1d82e520913396bc004501
evan-a-west <evan.a.west@gmail.com>
2023-07-22 18:08:23 -0400

    Data and some tagging

--- src/test/java/google/CucumberTestSuite_Google.java
+++ src/test/java/google/CucumberTestSuite_Google.java
@@ -4,8 +4,13 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+import io.cucumber.junit.CucumberOptions;
+
 @Suite
 @IncludeEngines("cucumber")
+@CucumberOptions(
+    tags = "@GoogleSearch"
+)
 @SelectClasspathResource("/features/google")
 public class CucumberTestSuite_Google {
 }
--- src/test/resources/features/google/search_for_cfo.feature
+++ src/test/resources/features/google/search_for_cfo.feature
@@ -1,5 +1,6 @@
 Feature: Search by keyword
 
+  @GoogleSearch
   Scenario: Searching for a CFO of a company
     Given User is searching for the CFO of a company on google.com
     When they look up CFO info for "Duck Creek Technologies"
